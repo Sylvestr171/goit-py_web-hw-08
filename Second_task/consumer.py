9be52a898c9ecfd0f941668c995b588ff3fbdc39
Oleksii <user@example.com>
2025-08-03 17:16:50 +0300

    done

--- Second_task/consumer.py
+++ Second_task/consumer.py
@@ -1,7 +1,26 @@
 import os
 import sys
-
+from time import sleep
 import pika
+from bson import ObjectId
+from models import Contacts
+
+def send_email(data):
+    sleep(1)
+    return data[2:26]
+
+def update_checkbox_to_true(id_for_change):
+    try:
+        obj_id = ObjectId(id_for_change)
+        document = Contacts.objects(id=obj_id).first()
+        if document:
+            document.Checkbox = True
+            document.save()
+            print("✅ MongoDB: checkbox оновлено на True")
+        else:
+            print("⚠️ Користувача з таким id не знайдено")
+    except Exception as e:
+        print(f"❌ Помилка оновлення: {e}")
 
 
 def main():
@@ -10,12 +29,16 @@ def main():
         pika.ConnectionParameters(host='localhost', port=5672, credentials=credentials))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='send_email')
 
     def callback(ch, method, properties, body):
-        print(f" [x] Received {body.decode()}")
+        data=body.decode()
+        print(f" [x] Received {data}")
+        id = send_email(data)
+        print (f">>>>>>{id}")
+        update_checkbox_to_true(id)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='send_email', on_message_callback=callback, auto_ack=True)
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
--- Second_task/producer.py
+++ Second_task/producer.py
@@ -12,8 +12,9 @@ channel = connection.channel()
 
 channel.queue_declare(queue='send_email')
 
+
 contact_list = Contacts.objects()
-contact_dict = {contact.id : [contact.fullname, contact.email, contact.Checkbox] for contact in contact_list}
+contact_dict = {contact.id : [str(contact.id), contact.fullname, contact.email, contact.Checkbox] for contact in contact_list}
 
 for key in contact_dict:
     data = contact_dict.get(key)
@@ -21,6 +22,6 @@ for key in contact_dict:
     message_byte = json_str.encode('utf-8')
 
     channel.basic_publish(exchange='', routing_key='send_email', body=message_byte)
-
     print(f" [x] Sent '{data}'")
+
 connection.close()
